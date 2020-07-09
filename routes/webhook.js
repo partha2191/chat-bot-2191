6c46d83c256b08f4c3cdffb0b82ece0d182af6f3
Partha Ghosh <p.ghosh2191@gmail.com>
2020-07-09 14:09:00 +0530

    Chat Bot

--- routes/webhook.js
+++ routes/webhook.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const request = require('request');
 
 router.get('/webhook', (req, res) => {
-  let query = req.query;
+  // let query = req.query;
   // console.log(query);
   // Your verify token. Should be a random string.
   let VERIFY_TOKEN = process.env.RANDOM_VERIFY_TOKEN;
@@ -42,6 +43,18 @@ router.post('/webhook', (req, res) => {
       // will only ever contain one message, so we get index 0
       let webhook_event = entry.messaging[0];
       console.log(webhook_event);
+
+      // Get the sender PSID
+      let sender_psid = webhook_event.sender.id;
+      console.log('Sender PSID: ' + sender_psid);
+
+      // Check if the event is a message or postback and
+      // pass the event to the appropriate handler function
+      if (webhook_event.message) {
+        handleMessage(sender_psid, webhook_event.message);        
+      } else if (webhook_event.postback) {
+        handlePostback(sender_psid, webhook_event.postback);
+      }
     });
 
     // Returns a '200 OK' response to all requests
@@ -52,5 +65,43 @@ router.post('/webhook', (req, res) => {
   }
 
 });
+
+function handleMessage(sender_psid, received_message) {
+  let response;
+  // Check if the message contains text
+  if (received_message.text) {    
+    // Create the payload for a basic text message
+    response = {
+      "text": `You sent the message: "${received_message.text}". Now send me an image!`
+    }
+  }
+  // Sends the response message
+  callSendAPI(sender_psid, response);
+}
+
+function callSendAPI(sender_psid, response) {
+  // Construct the message body
+  let request_body = {
+    "recipient": {
+      "id": sender_psid
+    },
+    "message": response
+  }
+
+  request.post({
+    uri: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: {
+      "access_token": process.env.PAGE_ACCESS_TOKEN
+    },
+    method: 'POST',
+    json: request_body,
+  }, (err, res, body) => {
+    if (!err) {
+      console.log('message sent!');
+    } else {
+      console.error("Unable to send message:" + err);
+    }
+  });
+}
 
 module.exports = router;
